393f715c42a83ef5bf13fa236901fecc0aab01d1
idontknowwhatotodolol101 <salmamohamud15@Gmail.com>
2024-10-30 10:06:01 +0000

    renaming api

--- amplify/backend/api/wastemanagementapiyoutube/override.ts
+++ amplify/backend/api/wastemanagementapiyoutube/override.ts
@@ -1,29 +1,39 @@
+// This file is used to override the REST API resources configuration
 import { AmplifyApiRestResourceStackTemplate, AmplifyProjectInfo } from '@aws-amplify/cli-extensibility-helper';
 
 export function override(resources: AmplifyApiRestResourceStackTemplate, amplifyProjectInfo: AmplifyProjectInfo) {
-  resources.addCfnResource(
-    {
-      type: "AWS::Lambda::Permission",
-      properties: {
-        Action: "lambda:InvokeFunction",
-        FunctionName: { "Ref": "wastemanagmentfunctionyoutube2" },
-        Principal: "apigateway.amazonaws.com",
-        SourceArn: {
-          "Fn::Join": [
-            "",
-            [
-              "arn:aws:execute-api:",
-              { "Ref": "AWS::Region" },
-              ":",
-              { "Ref": "AWS::AccountId" },
-              ":",
-              { "Ref": "wastemanagementapiyoutube" },
-              "/*/*"
+
+// Adding Resource Based policy to Lambda authorizer function
+resources.addCfnResource(
+  {
+    type: "AWS::Lambda::Permission",
+    properties: {
+      Action: "lambda:InvokeFunction",
+      FunctionName: {"Ref": "<wastemanagmentfunctionyoutube2>"},
+      Principal: "apigateway.amazonaws.com",
+      SourceArn:{
+        "Fn::Join": [
+          "",
+          [
+            "arn:aws:execute-api:",
+            {
+              "Ref": "eu-west-2"
+            },
+            ":",
+            {
+              "Ref": "084375567719"
+            },
+            ":",
+            {
+              "Ref": "<wastemanagementapiyoutube> "
+            },
+            "/*/*"
             ]
           ]
         }
       }
     },
     "LambdaAuthorizerResourceBasedPolicy"
-  );
+);
+
 }
