646967d4b4a611f9860d12a4d29f6d162f09e060
idontknowwhatotodolol101 <salmamohamud15@Gmail.com>
2024-10-30 09:08:13 +0000

    adding api function changes

---
+++ amplify/backend/api/wastemanagementapiyoutube/override.ts
@@ -0,0 +1,39 @@
+// This file is used to override the REST API resources configuration
+import { AmplifyApiRestResourceStackTemplate, AmplifyProjectInfo } from '@aws-amplify/cli-extensibility-helper';
+
+export function override(resources: AmplifyApiRestResourceStackTemplate, amplifyProjectInfo: AmplifyProjectInfo) {
+
+// Adding Resource Based policy to Lambda authorizer function
+resources.addCfnResource(
+  {
+    type: "AWS::Lambda::Permission",
+    properties: {
+      Action: "lambda:InvokeFunction",
+      FunctionName: {"Ref": "<wastemanagmentfunctionyoutube2>"},
+      Principal: "apigateway.amazonaws.com",
+      SourceArn:{
+        "Fn::Join": [
+          "",
+          [
+            "arn:aws:execute-api:",
+            {
+              "Ref": "eu-west-2"
+            },
+            ":",
+            {
+              "Ref": "084375567719"
+            },
+            ":",
+            {
+              "Ref": "<wastemanagmentapiyoutube> "
+            },
+            "/*/*"
+            ]
+          ]
+        }
+      }
+    },
+    "LambdaAuthorizerResourceBasedPolicy"
+);
+
+}
